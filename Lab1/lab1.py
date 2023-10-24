069b5b5afa31b78bdd81dfefca0f2544da090108
Dulea <mihaidulea@yahoo.com>
2023-10-24 16:41:15 +0300

    labs 1 and 2

---
+++ Lab1/lab1.py
@@ -0,0 +1,70 @@
+import random, math
+
+class GaussAI:
+    points_number = 10000
+    x_plus = 300
+    x_minus = x_plus * -1
+
+    y_plus = 300
+    y_minus = y_plus * -1
+
+    zones = [
+        {'mx': 180, 'ox': 10, 'my': 220, 'oy': 10, 'color': 'red'},
+        {'mx': -100, 'ox': 5, 'my': 50, 'oy': 5, 'color': 'green'},
+        {'mx': -200, 'ox': 10, 'my': 100, 'oy': 10, 'color': 'blue'},
+        {'mx': -210, 'ox': 5, 'my': -100, 'oy': 5, 'color': 'yellow'},
+        {'mx': 150, 'ox': 10, 'my': -150, 'oy': 10, 'color': 'pink'}
+    ]
+
+    def __create_file(self):
+         file = open("output.txt", "w")
+         
+         return file
+     
+    def get_gauss(self, coordinate, zone):
+        key = list(coordinate.keys())[0]
+        
+        numerator = ((zone[f'm{key}'] - coordinate[key]) ** 2)
+        denominator = (2 * (zone[f'o{key}'] ** 2))
+       
+        fraction =  -1 * (numerator / denominator)
+        gauss_result = math.e ** fraction
+        
+        return gauss_result
+
+    def is_valid(self, gauss_value):
+        probability = round(random.uniform(0, 1), 3)
+
+        return gauss_value > probability
+
+    #Flush the file every time!
+    def main(self):
+        count = 0
+        file = self.__create_file()
+
+        while count < self.points_number:
+            zone_number = random.randint(0, len(self.zones) - 1)
+            zone = self.zones[zone_number]
+
+            x_gauss = 0
+            y_gauss = 0
+
+            x_coord =  {}
+            y_coord = {}
+
+            while not self.is_valid(x_gauss):
+                x_coord = {'x': random.randint(self.x_minus, self.x_plus)}
+                x_gauss = self.get_gauss(x_coord, zone)
+
+            while not self.is_valid(y_gauss):
+                y_coord = {'y': random.randint(self.y_minus, self.y_plus)}
+                y_gauss = self.get_gauss(y_coord, zone)
+
+            file.write(f'{x_coord["x"]} {y_coord["y"]} {zone["color"]}\n')
+            count += 1
+
+        file.close()
+
+if __name__ == '__main__':
+    gauss_ai = GaussAI()
+    gauss_ai.main()
---
+++ Lab2/lab2.py
@@ -0,0 +1,103 @@
+import random
+import sys
+from math import sqrt
+from collections import defaultdict
+
+
+class KMeans():
+    
+    centroid_number = random.randint(2, 10)
+    points = []
+    centroids = []
+    clusters = defaultdict(list)
+    
+    def __init__(self):
+        self.get_coordinates()
+        self.set_centroizi_coordinates()
+ 
+
+    def get_coordinates(self): 
+        with open('output.txt', 'r') as file:
+            for line in file:
+                elements = line.split()
+                x = int(elements[0])
+                y = int(elements[1])
+                
+                coords = {'x' : x, 'y' : y}
+                
+                self.points.append(coords)
+                
+                
+    def set_centroizi_coordinates(self):
+        for centroid_number in range(self.centroid_number):
+            x = random.randint(-300, 300)
+            y = random.randint(-300, 300)
+            
+            coords = {'x' : x, 'y' : y}
+            centroid = {centroid_number : coords}
+            self.centroids.append(centroid)
+    
+    
+    def get_euclidian_distance(self, point, centroid):
+        sum = 0
+        for point_coordinate, centroid_coordinate in zip(point.keys(), centroid.keys()):
+            sum += (point[point_coordinate] - centroid[centroid_coordinate]) ** 2
+            
+        euclidian_distance = sqrt(sum)
+        
+        return round(euclidian_distance, 4)
+
+
+    def group_points(self):
+        for point in self.points:
+            min_distance = sys.maxsize
+            target_centroid = -1
+            
+            for centroid in self.centroids:
+                centroid_number = list(centroid.keys())[0]
+                centroid_coords = list(centroid.values())[0]
+                
+                distance = self.get_euclidian_distance(point, centroid_coords)
+                
+                if distance < min_distance:
+                    min_distance = distance
+                    target_centroid = centroid_number 
+            
+            point['distance'] = min_distance
+            self.clusters[target_centroid].append(point)
+            
+            
+    def get_weight_center(self, points):
+        
+        x_weight_center = 0
+        y_weight_center = 0 
+        
+        for point in points:
+            x_weight_center += point['x']
+            y_weight_center += point['y']
+        
+        x_weight_center /= len(points)
+        y_weight_center /= len(points)
+        
+        new_centroid = {'x' :round(x_weight_center, 4), 'y' : round(y_weight_center, 4)}
+        
+        return new_centroid
+   
+         
+    def modify_centroids_coordinates(self):
+        for centroid_number in range(self.centroid_number):
+            cluster = self.clusters[centroid_number]
+            new_centroid_coordinates = self.get_weight_center(cluster)
+            self.centroids[centroid_number] = new_centroid_coordinates
+            
+    def main(self):
+        self.group_points()
+        self.modify_centroids_coordinates()
+        
+        
+#pentru fiecare cluster cu i incepand de la 1
+if __name__ == '__main__':
+    kmeans = KMeans()
+ 
+    kmeans.main()
+    print(kmeans.clusters)
