a2184426c0ff36a05d38d6b189c7d4df9d4117b8
Dulea <duleasoft@gmail.com>
2023-11-26 18:28:08 +0200

    deleted trailing spaces

--- Lab3/window_lab3.py
+++ Lab3/window_lab3.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 class Window:
     x = []
     y = []
-    
+
     x_max = 300
     x_min = -x_max
 
     y_max = 300
     y_min = -y_max
-    
+
     fig, ax = plt.subplots(figsize=(3, 3))
-    
+
     def __init__(self):
         self.ax.set_aspect('equal')
 
         self.ax.set_xlim(self.x_min, self.x_max)
         self.ax.set_ylim(self.y_min, self.x_max)
 
-        self.ax.set_xticks([])  
+        self.ax.set_xticks([])
         self.ax.set_yticks([])
 
     def set_points(self, points):
         x = []
         y = []
-        
+
         for point in points:
             x.append(point['x'])
             y.append(point['y'])
@@ -34,7 +34,7 @@ class Window:
     def set_neurons(self, neurons):
         x = []
         y = []
-        
+
         for row in neurons:
             for neuron in row:
                 x.append(neuron['x'])
@@ -46,20 +46,20 @@ class Window:
         for row in neurons:
             xs = []
             ys = []
-            
+
             for neuron in row:
                 xs.append(neuron['x'])
                 ys.append(neuron['y'])
-            
-            plt.plot(xs, ys, color='blue', linewidth=0.5)    
-            plt.plot(ys, xs, color='blue', linewidth=0.5) 
+
+            plt.plot(xs, ys, color='blue', linewidth=0.5)
+            plt.plot(ys, xs, color='blue', linewidth=0.5)
 
     def draw_points(self, x = [], y = [], color='black', strength=1, label=''):
         self.ax.scatter(x=x, y=y, color=color, marker='o', s=strength)
         self.ax.legend(label, loc='upper left')
-    
+
     def open_window(self, block=True):
         plt.show(block=block)
-        
+
     def clear_window(self):
         self.ax.clear()
